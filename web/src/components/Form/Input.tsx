7f6f2963d0a9a20808a2609c7d976823d50b75eb
davi <davinunes.in@gmail.com>
2022-09-18 15:05:52 -0300

    feat: add request to show games and modal to create ads

---
+++ web/src/components/Form/Input.tsx
@@ -0,0 +1,9 @@
+import {InputHTMLAttributes} from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
+
+export function Input(props: InputProps) {
+    return (
+        <input {...props} className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:zinc-500" type="text" placeholder="Selecione o game que deseja jogar" />
+    )
+}
